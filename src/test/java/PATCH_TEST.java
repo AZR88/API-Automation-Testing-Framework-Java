097b145824b2530591b222be8e97a707602a1793
AZR88 <azrielamaldany@gmail.com>
2024-08-08 00:19:32 +0700

    ADD Patch Test DAta

---
+++ src/test/java/PATCH_TEST.java
@@ -0,0 +1,24 @@
+import apiauto.APITest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class PATCH_TEST {
+    @DataProvider(name = "userData")
+    public Object[][] createUserData() {
+        return new Object[][] {
+                {2,"John Doe", "Software Developer", true},          // Positive case
+                {2,"", "QA Engineer", false},                        // Negative case: empty name
+                {2,"Alice", "", false},                              // Negative case: empty job
+                {2, "VeryLongNameThatExceedsNormalLengthaldhiuhisabdkauhiugfKJKASJHFKSJH", "Dev", false}, // Negative case: very long name
+                {2,"Bob", "Business Analyst", true},                 // Positive case
+                {2,"!@#$%^&*()", "Engineer", false},                 // Negative case: special characters in name
+                {3,"Jane Doe", "Tester", true}                       // Negative case
+        };
+    }
+
+    @Test(dataProvider = "userData")
+    public void testPutUser(Integer ID,String name, String job, boolean shouldPass) {
+        APITest test = new APITest();
+        test.PatchUser(ID,name, job, shouldPass);
+    }
+}
